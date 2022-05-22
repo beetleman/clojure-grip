186a4a39088fedef4d49d43d4ca443f2924192de
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2022-05-22 16:07:07 +0200

    add test server

--- src/user.clj
+++ src/user.clj
@@ -1,1 +1,20 @@
-(ns user)
+(ns user
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]))
+
+
+(defn handler
+  [_request]
+  (response/response "Hello!"))
+
+
+(defonce server (atom nil))
+
+
+(swap! server
+  (fn [s]
+    (when s
+      (.stop s))
+    (jetty/run-jetty #(handler %)
+                     {:port  3000
+                      :join? false})))
